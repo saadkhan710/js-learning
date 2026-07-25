355e2257e863e3e731872e6dd6f54e6f049e34f7
saadkhan710 <70254602+saadkhan710@users.noreply.github.com>
2026-07-25 13:57:19 +0000

    Add 01_DataTypes

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,24 @@
+// alert ( 3+3) // we are using node.js not browser
+
+let name = "Saad"
+let age = 24
+let isLoggedIn = true 
+
+// number 2 to power of 53
+// bigInt If the no is too big : Stocks 
+// string 
+// boolean true/false 
+// null => its a type : representation of empty value 
+// undefined => value is not assigned at the moment
+// symbol => used to identify uniqueness of component 
+
+
+
+// Object 
+console.log(typeof null);  // return Object  Interview questions
+
+console.log(typeof undefined);  //return undefined  
+
+ // undefined is type just like number, string
+
+ // null type ---> Object 
