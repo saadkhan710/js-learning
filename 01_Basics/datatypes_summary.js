7039c18a0fe0f8698affd2f2bdf8eeb4082090ea
saadkhan710 <70254602+saadkhan710@users.noreply.github.com>
2026-07-25 21:28:09 +0000

    DataTypes : Interview Questions

---
+++ 01_Basics/datatypes_summary.js
@@ -0,0 +1,24 @@
+/*
+
+Javascript is dynamically type Language: 
+
+Let Score = 100
+Let name = "Saad"
+
+
+Data Types are classified 2 types: 
+
+Kis tar se memory mai rakha jata hai --> access kiya jata hai 
+
+1. Primitive ( call by value : Copy is given)
+
+7 types: String, Number, Boolean, null (empty), undefined, Symbol, BigInt
+
+
+
+2. Non Primitive / reference type (call by ref: Address is given)
+
+Array, Objects, Functions
+
+
+*/
