41d32543ae472fdf32fdfa29ec491ac1c4a2d293
saadkhan710 <70254602+saadkhan710@users.noreply.github.com>
2026-07-26 09:05:21 +0000

    updated datatype summary

--- 01_Basics/datatypes_summary.js
+++ 01_Basics/datatypes_summary.js
@@ -35,3 +35,38 @@ const myFunction = function(){
     console.log("Hello W")
 }
 
+/*
+
+Stack memory (we get copy): Primitive
+
+
+
+Heap memory (we get Address): Non Primitive 
+
+
+*/
+
+let username = "saad"
+
+let username2 = username
+
+username2 = "paad"
+
+console.log(username2)
+console.log(username)
+
+
+
+
+let userOne = {
+    email : "saadzz.khan@gmail.com",
+    upi   : "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.upi = "user2@ybl.com"
+
+console.log(userOne.upi)   // same putput for both user2@ybl.com
+console.log(userTwo.upi)
+
