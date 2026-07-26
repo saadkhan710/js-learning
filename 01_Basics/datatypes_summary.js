8b1509bed0af6d4c9b3ede5ddc3b85fbe3c724d5
saadkhan710 <70254602+saadkhan710@users.noreply.github.com>
2026-07-26 08:17:27 +0000

    Add datatypes_summary Interview

--- 01_Basics/datatypes_summary.js
+++ 01_Basics/datatypes_summary.js
@@ -14,11 +14,24 @@ Kis tar se memory mai rakha jata hai --> access kiya jata hai
 
 7 types: String, Number, Boolean, null (empty), undefined, Symbol, BigInt
 
-
+typeof(null) -> Object
 
 2. Non Primitive / reference type (call by ref: Address is given)
 
-Array, Objects, Functions
+Array, Objects, Functions : Return type Function Object /  Object 
 
 
 */
+
+
+const heros = ["Shaktiman","Doga","naaraj"]
+
+let myObj = {
+    name : "Saad",      // Object is always in {} & Key Value pair
+    age : "22"
+}
+
+const myFunction = function(){
+    console.log("Hello W")
+}
+
