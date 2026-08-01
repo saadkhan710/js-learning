c41cab60867102ea1455a4eb93da7f0876e227e4
saadkhan710 <70254602+saadkhan710@users.noreply.github.com>
2026-08-01 15:38:50 +0000

    Add Strings

---
+++ 01_Basics/05_Strings.js
@@ -0,0 +1,18 @@
+// Modern String Syntax : String inter Pollination
+
+const name = "Saad"
+
+const repoCount = 5
+
+console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`)
+
+
+
+const gameName = new String('SaadKhan')
+
+console.log(gameName[0])
+console.log(gameName.toLocaleLowerCase())
+console.log(gameName.length)
+console.log(gameName.charAt(1))
+
+// Doen't change the original string 
