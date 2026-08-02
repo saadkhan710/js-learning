401b4729163619d198471ea8bbf2828f0eb25cb4
saadkhan710 <70254602+saadkhan710@users.noreply.github.com>
2026-08-02 10:54:23 +0000

    Add more String functions

--- 01_Basics/05_Strings.js
+++ 01_Basics/05_Strings.js
@@ -8,7 +8,7 @@ console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`)
 
 
 
-const gameName = new String('SaadKhan')
+const gameName = new String('Saad-Khan-07')
 
 console.log(gameName[0])
 console.log(gameName.toLocaleLowerCase())
@@ -16,3 +16,20 @@ console.log(gameName.length)
 console.log(gameName.charAt(1))
 
 // Doen't change the original string 
+
+const movieName = "hiteshc"
+console.log(movieName.length)
+
+
+const newString = movieName.substring(0,2)
+console.log(newString)
+
+const anotherString = movieName.slice(-8,5)
+console.log(anotherString)
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20','-'))
+console.log(url.includes('hitesh'))
+
+console.log(gameName.split("-"))
