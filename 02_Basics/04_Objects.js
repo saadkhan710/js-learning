2d356114cd44d798d11bbac76c2549fee70ac695
saadkhan710 <70254602+saadkhan710@users.noreply.github.com>
2026-08-22 10:18:33 +0000

    Add Object 4

--- 02_Basics/04_Objects.js
+++ 02_Basics/04_Objects.js
@@ -1,21 +1,56 @@
 // Singleton 
 
 const tinderUSer = new Object()   // Singelton Object exactly same 
-tinderUSer.id = "23abc"
-tinderUSer.name = "Sam"
-tinderUSer.isLoggedIN = false
 
-console.log(tinderUSer)
+tinderUSer.id = "123abc"
+tinderUSer.name = "Sammy"
+tinderUSer.isLoggedIn = false 
+
+// console.log(tinderUSer)
 
 
-const user1 = {
-    email : "sam@gmail.com",
+
+const regularUSer = {
+    email : "some@gmail.com",
     fullname : {
-        userfullname : {
-            firstname:"hitesh",
-            lastname:"chowk"
+        userfullname :{
+            firstname:"hitest",
+            lastname:"choudary"
         }
     }
 }
 
-console.log(user1.fullname.userfullname.lastname)
+console.log(regularUSer.fullname.userfullname.lastname);
+
+const obj1 = { 1:"a", 2:"b"}
+const obj2 = { 3:"c", 4:"d"}
+
+const obj3 = Object.assign({}, obj1,obj2)     // {target {}, source }
+// console.log(obj3)
+
+
+const obj4 = {...obj1, ...obj2}     // other way to do same thing : easy 
+console.log(obj4)
+
+// accessing the data from Database : sample 
+
+const arr =[
+    {
+        id : "1",
+        name : "Saad"
+    },
+    {
+        id : "2",
+        name : "Neha"
+    }
+]
+
+console.log(arr[0].id)
+
+console.log(tinderUSer);
+
+console.log(Object.keys(tinderUSer))  // Returns keys of objects : In Array [ 'id', 'name', 'isLoggedIn' ]
+
+console.log(Object.values(tinderUSer))  // Returns values of objects : In Array
+
+console.log(tinderUSer.hasOwnProperty("name"))  // check if object has that Key
