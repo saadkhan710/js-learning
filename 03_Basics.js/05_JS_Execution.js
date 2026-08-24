464e32054a3c0275cb225e2accd4c29162959eba
saadkhan710 <70254602+saadkhan710@users.noreply.github.com>
2026-08-24 10:05:49 +0000

    Add Java Execution : Step by Step

---
+++ 03_Basics.js/05_JS_Execution.js
@@ -0,0 +1,24 @@
+// Javascript Execution Context 
+
+// Javascript is single threaed
+
+
+// 1. Javascript will always run your file in 2 phases
+
+// 2. Whenever JS file is given to run -> First thing it creates GLobal Execution context -> its given to variable called this ( as refernce )
+
+// 2.1 Browser : Global Execution context will be Windows Object 
+// 2.2 Node etc : It will be empty 
+
+// 3. GLobal Execution context
+// 4. Functional Execution Context
+
+
+// 5. Js File runs in 2 Phases 
+
+// 1. Memory creation Phase : Sirf jagah alloct hoti ( All varibles.. )
+// 2. Execution Phase 
+
+
+
+
